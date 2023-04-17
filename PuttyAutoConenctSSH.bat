fa859f870d11964cca4c53109affb95df2f6927a
CHEN,BO-HAN <fq33578@gmail.com>
2023-04-17 15:54:44 +0800

    Update PuttyAutoConenctSSH.bat

--- PuttyAutoConenctSSH.bat
+++ PuttyAutoConenctSSH.bat
@@ -1,2 +1,6 @@
+::1. Connect SSH & Run Command
 putty.exe -ssh root@10.46.73.20 -l -P 22 -pw PASSWORD -m C:/RCS_SSH_Command/reboot.txt
 putty.exe -ssh root@10.46.73.20 -l -P 22 -pw PASSWORD -m C:/RCS_SSH_Command/poweroff.txt
+
+::2. Remove SSH Key
+ssh-keygen -R IP
